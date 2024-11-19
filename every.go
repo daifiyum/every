9a984b64438f1e0c6bca70f5407bb5bd528c5fdd
daifiyum <daiqisen131@gmail.com>
2024-11-19 12:14:22 +0800

    fix

--- every.go
+++ every.go
@@ -8,13 +8,13 @@ import (
 	"time"
 )
 
-// Task 定义一个定时任务结构体
 type Task struct {
-	duration time.Duration  // 任务执行的间隔
-	taskFunc func()         // 定时执行的任务
-	timer    *time.Timer    // 定时器 timer，用于周期性执行任务
-	stopChan chan struct{}  // 停止任务的信号通道
-	wg       sync.WaitGroup // 用于等待任务完成
+	duration   time.Duration      // 当前任务的执行间隔
+	taskFunc   func()             // 定时任务的逻辑
+	timer      *time.Timer        // 用于控制定时任务的 Timer
+	stopChan   chan struct{}      // 停止信号
+	updateChan chan time.Duration // 更新间隔信号
+	wg         sync.WaitGroup     // 用于等待任务退出
 }
 
 // NewTask 创建一个新的定时任务
@@ -25,17 +25,17 @@ func NewTask(interval string, task func()) (*Task, error) {
 	}
 
 	return &Task{
-		duration: duration,
-		taskFunc: task,
-		stopChan: make(chan struct{}),
+		duration:   duration,
+		taskFunc:   task,
+		stopChan:   make(chan struct{}),
+		updateChan: make(chan time.Duration),
 	}, nil
 }
 
-// parseDuration 解析时间格式
+// parseDuration 解析时间间隔
 func parseDuration(interval string) (time.Duration, error) {
 	var totalDuration time.Duration
 
-	// 解析分钟、小时、天和秒
 	for _, part := range strings.Split(interval, ",") {
 		part = strings.TrimSpace(part)
 		if len(part) == 0 {
@@ -71,7 +71,8 @@ func (t *Task) Start() {
 	go func() {
 		defer t.wg.Done()
 
-		t.timer = time.NewTimer(t.duration) // 创建 Timer
+		// 初始化定时器
+		t.timer = time.NewTimer(t.duration)
 
 		for {
 			select {
@@ -79,32 +80,36 @@ func (t *Task) Start() {
 				// 停止任务
 				t.timer.Stop()
 				return
+			case newDuration := <-t.updateChan:
+				// 更新间隔时间并重置定时器
+				t.timer.Stop()
+				t.duration = newDuration
+				t.timer.Reset(t.duration)
 			case <-t.timer.C:
 				// 执行任务
 				t.taskFunc()
 
-				// 激活定时器，并使用新的间隔
+				// 重置定时器
 				t.timer.Reset(t.duration)
 			}
 		}
 	}()
 }
 
-// Stop 停止定时任务
+// Stop 停止任务
 func (t *Task) Stop() {
 	close(t.stopChan)
 	t.wg.Wait()
 }
 
-// UpdateInterval 动态更新定时任务的间隔
+// UpdateInterval 更新任务间隔时间
 func (t *Task) UpdateInterval(interval string) error {
-	// 解析新的间隔时间
-	duration, err := parseDuration(interval)
+	newDuration, err := parseDuration(interval)
 	if err != nil {
 		return err
 	}
 
-	// 更新任务的间隔
-	t.duration = duration
+	// 发送更新信号
+	t.updateChan <- newDuration
 	return nil
 }
